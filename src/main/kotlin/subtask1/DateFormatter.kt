83919e7f4be0d04b3370c7dfb0e9a9df377271da
Natallia Vasilyeva <nn1.vasilyeva@gmail.com>
2020-04-09 10:46:11 +0300

    feat: add code to dateFormatter subtask

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,29 @@
 package subtask1
 
+import java.time.DateTimeException
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.TextStyle
+import java.util.*
+
 class DateFormatter {
 
-    // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+        var nDay = day.toInt();
+        var nMonth = month.toInt();
+        var nYear = year.toInt();
+        return try {
+            var nDate = LocalDate.of(nYear, nMonth, nDay);
+            var formatter = DateTimeFormatter.ofPattern("dd MMMM")
+            var formattedDate = nDate.format(formatter)
+
+            var dayOfWeek = nDate.dayOfWeek.getDisplayName(
+                TextStyle.FULL,
+                Locale.getDefault(Locale.Category.FORMAT)
+            )
+            return "$formattedDate, $dayOfWeek"
+        } catch (exception: DateTimeException) {
+            "Такого дня не существует"
+        }
     }
 }
