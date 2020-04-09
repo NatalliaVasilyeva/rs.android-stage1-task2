26dcfc1564e06a82033edc7271d0fd85466e2b2d
Natallia Vasilyeva <nn1.vasilyeva@gmail.com>
2020-04-09 11:25:33 +0300

    feat: add code to TimeConverter subtask

--- src/main/kotlin/subtask2/TimeConverter.kt
+++ src/main/kotlin/subtask2/TimeConverter.kt
@@ -2,8 +2,30 @@ package subtask2
 
 class TimeConverter {
 
-    // TODO: Complete the following function
     fun toTextFormat(hour: String, minute: String): String {
-        throw NotImplementedError("Not implemented")
+
+        val arrayOfTime = arrayOf("", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+        "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty",
+            "twenty one", "twenty two", "twenty three", "twenty four", "twenty five", "twenty six", "twenty seven",
+            "twenty eight", "twenty nine");
+
+        var minute=minute.toInt();
+        var hour = hour.toInt();
+
+        if (minute<0 || minute>60 || hour<0 || hour>24) {
+            return ""
+        }
+
+        return when (minute) {
+            0 -> "${arrayOfTime[hour]} o' clock"
+            1 -> "${arrayOfTime[minute]} minute past ${arrayOfTime[hour]}"
+            15 -> "quarter past ${arrayOfTime[hour]}"
+            30 -> "half past ${arrayOfTime[hour]}"
+            45 -> "quarter to ${arrayOfTime[hour+1]}"
+            59 -> "${arrayOfTime[60-minute]} minute to ${arrayOfTime[hour+1]}"
+            in 1..29 ->"${arrayOfTime[minute]} minutes past ${arrayOfTime[hour]}"
+            in 31..60 ->"${arrayOfTime[60-minute]} minutes to ${arrayOfTime[hour+1]}"
+            else ->""
+        }
     }
 }
