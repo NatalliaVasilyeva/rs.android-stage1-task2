b48a2c3ca1a307cbf9b35d154058c4b64c40f66a
Natallia Vasilyeva <nn1.vasilyeva@gmail.com>
2020-04-12 22:07:33 +0300

    feat: add code to Pangram subtask

--- src/main/kotlin/subtask4/Pangram.kt
+++ src/main/kotlin/subtask4/Pangram.kt
@@ -2,17 +2,71 @@ package subtask4
 
 class Pangram {
 
-    // TODO: Complete the following function
+    val arrayOfVowels = listOf('a', 'e', 'i', 'o', 'u', 'y')
+
     fun getResult(inputString: String): String {
-        throw NotImplementedError("Not implemented")
+        return if (isPangram(createCharSet(inputString))) {
+            takeRusultIsPangramSentence(inputString).joinToString(" ")
+        } else
+            takeRusultIsNotPangramSentence(inputString).joinToString(" ")
     }
 
 
+    private fun isPangram(set: Set<Char>): Boolean {
+        return set.size == 26
+    }
 
+    private fun createCharSet(sentences: String): Set<Char> {
+        return sentences.toLowerCase()
+            .replace("[^a-zA-Z]".toRegex(), "")
+            .toSet()
+    }
 
+    private fun splitString(inputString: String): List<String> {
+        return inputString.split(" ")
+            .filter { it.isNotBlank() }
+    }
 
+    private fun takeRusultIsPangramSentence(sentence: String): List<String> {
+        val splittedArrayOfWords = splitString(sentence);
+        val listOfWords = mutableListOf<String>()
+        for (eachWord in splittedArrayOfWords) {
+            var numberOfVoweInWord = 0;
+            var newWord = "";
+            for (eachChar in eachWord) {
+                newWord += if (eachChar.isLetter() && (arrayOfVowels.contains(eachChar)
+                            || (arrayOfVowels.contains(eachChar.toLowerCase())))
+                ) {
+                    numberOfVoweInWord++;
+                    eachChar.toUpperCase();
+                } else {
+                    eachChar;
+                }
+            }
+            listOfWords.add("$numberOfVoweInWord" + newWord);
+        }
+        return listOfWords.sortedBy { it[0] }
+    }
 
-
-
-
+    private fun takeRusultIsNotPangramSentence(sentence: String): List<String> {
+        val splittedArrayOfWords = splitString(sentence);
+        val listOfWords = mutableListOf<String>()
+        for (eachWord in splittedArrayOfWords) {
+            var numberOfConsonantsInWord = 0;
+            var newWord = "";
+            for (eachChar in eachWord) {
+                newWord += if (eachChar.isLetter() && !(arrayOfVowels.contains(eachChar) || (arrayOfVowels.contains(
+                        eachChar.toLowerCase()
+                    )))
+                ) {
+                    numberOfConsonantsInWord++;
+                    eachChar.toUpperCase();
+                } else {
+                    eachChar;
+                }
+            }
+            listOfWords.add("$numberOfConsonantsInWord" + newWord);
+        }
+        return listOfWords.sortedBy { it[0] }
+    }
 }
